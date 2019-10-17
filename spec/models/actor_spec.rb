6d0308a779b5215967d034f0baf5fd6f1227d874
Jelani Woods <jelani@firstdraft.com>
2019-10-17 18:11:27 +0000

    Add actor#characters spec

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -51,3 +51,56 @@ describe Actor, "#filmography" do
     expect(actor.filmography).to match_array([first_movie, third_movie, fifth_movie])
   end
 end
+
+
+describe Actor, "#characters" do
+  it "returns the characters that the actor played", points: 1 do
+    actor = Actor.new
+    actor.save
+
+    other_actor = Actor.new
+    other_actor.save
+
+    first_movie = Movie.new
+    first_movie.save
+
+    second_movie = Movie.new
+    second_movie.save
+
+    third_movie = Movie.new
+    third_movie.save
+
+    fourth_movie = Movie.new
+    fourth_movie.save
+
+    fifth_movie = Movie.new
+    fifth_movie.save
+
+    first_character = Character.new
+    first_character.actor_id = actor.id
+    first_character.movie_id = first_movie.id
+    first_character.save
+
+    second_character = Character.new
+    second_character.actor_id = actor.id
+    second_character.movie_id = third_movie.id
+    second_character.save
+
+    third_character = Character.new
+    third_character.actor_id = other_actor.id
+    third_character.movie_id = third_movie.id
+    third_character.save
+
+    fourth_character = Character.new
+    fourth_character.actor_id = other_actor.id
+    fourth_character.movie_id = fourth_movie.id
+    fourth_character.save
+
+    fifth_character = Character.new
+    fifth_character.actor_id = actor.id
+    fifth_character.movie_id = fifth_movie.id
+    fifth_character.save
+
+    expect(actor.characters).to match_array([first_character, second_character, fifth_character])
+  end
+end
